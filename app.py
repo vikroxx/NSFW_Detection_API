2048dc9fb8b61f963aebe14d3e42ca5bfdd0ec4f
vikroxx <vikrant.saxena94@gmail.com>
2023-04-10 01:58:23 +0530

    added face detection to nsfw

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # from api import predict, app
 # from api.functions import download_image, process_base64_image
-from functions import download_image, process_base64_image
+from functions import download_image, process_base64_image, detect_faces
 
 from config import PORT
 import os
@@ -13,38 +13,6 @@ app = FastAPI()
 
 model = predict.load_model('nsfw_detector/nsfw_model.h5')
 
-
-
-
-@app.get("/main")
-async def detect_nsfw(url: str):
-    return {'status' : 0}
-    if not url:
-        return {"ERROR": "URL PARAMETER EMPTY"}
-    image = await download_image(url)
-    if not image:
-        return {"ERROR": "IMAGE SIZE TOO LARGE OR INCORRECT URL"}
-    results = predict.classify(model, image)
-    os.remove(image)
-    hentai = results['data']['hentai']
-    sexy = results['data']['sexy']
-    porn = results['data']['porn']
-    drawings = results['data']['drawings']
-    neutral = results['data']['neutral']
-    if neutral >= 25:
-        results['data']['is_nsfw'] = False
-        return results
-    elif (sexy + porn + hentai) >= 70:
-        results['data']['is_nsfw'] = True
-        return results
-    elif drawings >= 40:
-        results['data']['is_nsfw'] = False
-        return results
-    else:
-        results['data']['is_nsfw'] = False
-        return results
-
-
 @app.post("/verify_nsfw")
 async def detect_nsfw_route(request: Request):
     
@@ -56,6 +24,10 @@ async def detect_nsfw_route(request: Request):
     data = await request.json()
     base64= data['base64']
     image = process_base64_image(base64)
+
+    faces = detect_faces(image)
+    num_faces = len(faces)
+    image = image/255.0
     image = np.expand_dims(image, axis=0)
 
     probs = predict.classify_nd(model, image)
@@ -70,6 +42,8 @@ async def detect_nsfw_route(request: Request):
         results['data']['is_nsfw'] = False
     else:
         results['data']['is_nsfw'] = True
+    results['data']['num_faces'] = num_faces
+
     return results
     
 
--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ import base64
 import numpy as np
 from PIL import Image
 from io import BytesIO
-
+import cv2
 
 MAX_IMAGE_SIZE = MAX_IMAGE_SIZE * 1000000
 
@@ -34,12 +34,6 @@ def pad_base64(encoded_image):
     return encoded_image
 
 def process_base64_image(encoded_image, target_size=(224, 224)):
-    # Ensure the base64 string has correct padding
-    # encoded_image = pad_base64(encoded_image)
-
-    # # Decode the base64 encoded image
-    # decoded_image = base64.b64decode(encoded_image)
-
     base64_data = encoded_image
     decoded_image = base64.b64decode(base64_data)
     # nparr = np.frombuffer(decoded_image, np.uint8)
@@ -54,11 +48,13 @@ def process_base64_image(encoded_image, target_size=(224, 224)):
     img_array = np.asarray(img)
 
     # Normalize the image data by dividing each pixel value by 255
-    img_array = img_array / 255.0
+    # img_array = img_array / 255.0
 
     return img_array
 
 
-
-
-
+def detect_faces(image):
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    return faces
