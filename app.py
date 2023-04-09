ece7d09acb98eab5a6cd7762573c12aee6884b09
vikroxx <vikrant.saxena94@gmail.com>
2023-04-09 18:33:17 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,75 @@
+# from api import predict, app
+# from api.functions import download_image, process_base64_image
+from functions import download_image, process_base64_image
+
+from config import PORT
+import os
+import uvicorn
+import numpy as np 
+from fastapi import FastAPI
+from nsfw_detector import predict
+
+app = FastAPI()
+
+model = predict.load_model('nsfw_detector/nsfw_model.h5')
+
+
+
+
+@app.get("/main")
+async def detect_nsfw(url: str):
+    return {'status' : 0}
+    if not url:
+        return {"ERROR": "URL PARAMETER EMPTY"}
+    image = await download_image(url)
+    if not image:
+        return {"ERROR": "IMAGE SIZE TOO LARGE OR INCORRECT URL"}
+    results = predict.classify(model, image)
+    os.remove(image)
+    hentai = results['data']['hentai']
+    sexy = results['data']['sexy']
+    porn = results['data']['porn']
+    drawings = results['data']['drawings']
+    neutral = results['data']['neutral']
+    if neutral >= 25:
+        results['data']['is_nsfw'] = False
+        return results
+    elif (sexy + porn + hentai) >= 70:
+        results['data']['is_nsfw'] = True
+        return results
+    elif drawings >= 40:
+        results['data']['is_nsfw'] = False
+        return results
+    else:
+        results['data']['is_nsfw'] = False
+        return results
+
+
+@app.get("/verify_nsfw")
+async def detect_nsfw_route(base64: str):
+    # if not url:
+    #     return {"ERROR": "URL PARAMETER EMPTY"}
+    # image = await download_image(url)
+    # if not image:
+    #     return {"ERROR": "IMAGE SIZE TOO LARGE OR INCORRECT URL"}
+    image = process_base64_image(base64)
+    image = np.expand_dims(image, axis=0)
+
+    probs = predict.classify_nd(model, image)
+    results = dict(zip(['data'], probs))
+    hentai = results['data']['hentai']
+    sexy = results['data']['sexy']
+    porn = results['data']['porn']
+    drawings = results['data']['drawings']
+    neutral = results['data']['neutral']
+
+    if neutral >= 90:
+        results['data']['is_nsfw'] = False
+    else:
+        results['data']['is_nsfw'] = True
+    return results
+    
+
+
+# if __name__ == "__main__":
+#     uvicorn.run("api:app", host="0.0.0.0", port=PORT, log_level="info", reload = True)
---
+++ functions.py
@@ -0,0 +1,57 @@
+from config import MAX_IMAGE_SIZE
+from random import randint
+import aiohttp
+import aiofiles
+
+import base64
+import numpy as np
+from PIL import Image
+from io import BytesIO
+
+
+MAX_IMAGE_SIZE = MAX_IMAGE_SIZE * 1000000
+
+
+async def download_image(url):
+    file_name = f"{randint(6969, 6999)}.jpg"
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                if int(resp.headers['Content-Length']) > MAX_IMAGE_SIZE:
+                    return False
+                f = await aiofiles.open(file_name, mode='wb')
+                await f.write(await resp.read())
+                await f.close()
+            else:
+                return False
+    return file_name
+
+
+# def pad_base64(encoded_image):
+#     padding = 4 - (len(encoded_image) % 4)
+#     if padding < 4:
+#         encoded_image += "=" * padding
+#     return encoded_image
+
+def process_base64_image(encoded_image, target_size=(224, 224)):
+    # Ensure the base64 string has correct padding
+    # encoded_image = pad_base64(encoded_image)
+
+    # Decode the base64 encoded image
+    decoded_image = base64.b64decode(encoded_image)
+
+    # Convert the decoded image bytes to a PIL image object
+    img = Image.open(BytesIO(decoded_image))
+
+    # Resize the image to the target size
+    img = img.resize(target_size, Image.NEAREST)
+
+    # Convert the PIL image object to a NumPy array
+    img_array = np.asarray(img)
+
+    # Normalize the image data by dividing each pixel value by 255
+    img_array = img_array / 255.0
+
+    return img_array
+
+
